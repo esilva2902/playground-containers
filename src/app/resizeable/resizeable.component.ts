82d1d0043f9beaadd9aca2f20e906aa4bf3a9ba4
erick.silva <ericksilva@localhost.localdomain>
2019-07-12 21:41:26 -0500

    v0.0.5 - Printing some logs.

--- src/app/resizeable/resizeable.component.ts
+++ src/app/resizeable/resizeable.component.ts
@@ -15,18 +15,30 @@ export class ResizeableComponent implements OnInit {
 
   }
 
-  onResize(outerDiv: HTMLElement, rightDiv: HTMLElement, lastDiv: HTMLElement, e: MouseEvent) {
+  onResize(firstDiv: HTMLElement, rightDiv: HTMLElement, lastDiv: HTMLElement, e: MouseEvent) {
     e.preventDefault();
 
-    let original_width = outerDiv.getBoundingClientRect().right - outerDiv.getBoundingClientRect().left;
+    console.log(`MouseEvent(mousedown): `, e);
+    console.log(`original_mouse_x: `, e.pageX);
+    console.log(`firstDiv.getBoundingClientRect().right: `, firstDiv.getBoundingClientRect().right);
+    console.log(`firstDiv.getBoundingClientRect().left: `, firstDiv.getBoundingClientRect().left);
+
+    let original_width = firstDiv.getBoundingClientRect().right - firstDiv.getBoundingClientRect().left;
     let original_mouse_x = e.pageX;
+
+    console.log(`firstDiv.getBoundingClientRect().right - firstDiv.getBoundingClientRect().left`, original_width);
 
     let right_width = rightDiv.getBoundingClientRect().right - rightDiv.getBoundingClientRect().left;
     let last_width = lastDiv.getBoundingClientRect().right - lastDiv.getBoundingClientRect().left;
 
     this.resize = (moveEvent: MouseEvent) => {
+      console.log(`MouseEvent(mousemove): `, e);
+
       let width = original_width + (moveEvent.pageX - original_mouse_x);
-      outerDiv.style.width = `${width}px`;
+
+      console.log(`new firstDiv width: `, width);
+
+      firstDiv.style.width = `${width}px`;
 
       rightDiv.style.left = `${100 + width + 20}px`;
       lastDiv.style.left = `${100 + width + 20 + right_width + 20}px`;
